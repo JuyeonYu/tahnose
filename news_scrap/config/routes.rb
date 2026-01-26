e2e1344012fe567db9ef156613c496ad7edadfa8
juyeon <remake867@gmail.com>
2026-01-27 02:35:52 +0900

    chore

--- news_scrap/config/routes.rb
+++ news_scrap/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  root 'home#index'
+  # root 'home#index'
   resources :keywords do
     member do
       patch :toggle_alarm
