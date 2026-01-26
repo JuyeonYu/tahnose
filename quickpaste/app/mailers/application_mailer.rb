b1571cfaeefe75d36991f109288d8be02aeb6f5b
juyeon <remake867@gmail.com>
2026-01-26 14:08:44 +0900

    Configure Resend mail delivery

--- quickpaste/app/mailers/application_mailer.rb
+++ quickpaste/app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: ENV["RESEND_FROM"] || Rails.application.credentials.dig(:resend, :from) || "from@example.com"
   layout "mailer"
 end
