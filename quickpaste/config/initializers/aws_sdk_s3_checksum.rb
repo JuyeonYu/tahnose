a3ee387045ba37ae22224bd854810a55aad54f6e
juyeon <remake867@gmail.com>
2026-01-27 23:28:10 +0900

    chore

--- quickpaste/config/initializers/aws_sdk_s3_checksum.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Aws.config.update(
-  s3: {
-    # R2 호환: AWS SDK가 x-amz-checksum-* 를 추가로 붙이지 않게 함
-    request_checksum_calculation: "when_required",
-    response_checksum_validation: "when_required"
-  }
-)
